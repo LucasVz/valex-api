69686e46502b2980a50ad7bdbf1690f81b80b1f1
Marcus <marcusmoraes2010@hotmail.com>
2022-04-26 09:34:20 -0300

    Preparando revisão

--- schemas/paymentSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const paymentSchema = Joi.object({
-    password: Joi.string().regex(/[0-9]{4}/).required(),
-    amount: Joi.number().min(1).required(),
-});
-
-export default paymentSchema;
--- src/services/authService.ts
+++
@@ -1,7 +0,0 @@
-import * as authRepository from "../repositories/companyRepository.js"
-
-export async function validateCompany(apiKey : string){
-    const validCompany = await authRepository.findByApiKey(apiKey)
-    if(!validCompany) throw {error_type : "unauthorized_company", message: "the company does not is authorized"}
-    return validCompany
-}
